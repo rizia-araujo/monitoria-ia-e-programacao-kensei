e695e3e51137994bd3e0a01541067f2e02d1a993
Rízia Araujo <rizia.araujo12@gmail.com>
2026-04-14 20:59:39 -0300

    Atualizando alguns exemplos e exercícios da semana 02

---
+++ semana-02/ola.py
@@ -0,0 +1,6 @@
+# Meu (quase) primeiro programa Python!
+print("Ola, Kensei!")
+print("Eu sou", "um futuro dev")
+# Perguntando o nome do usuario
+nome = input("Qual seu nome? ")
+print("Bem-vindo,", nome, "!")
---
+++ semana-02/variaveis_caixas_com_etiquetas.py
@@ -0,0 +1,14 @@
+# Guardando valores em variáveis
+nome = "Jose" # texto (string)
+idade = 25 # numero inteiro (int)
+altura = 1.75 # numero decimal (float)
+estudante = True # verdadeiro/falso (bool)
+# Usando as variaveis
+print("Nome:", nome)
+print("Idade:", idade)
+print("Estudante?", estudante)
+# Minhas variáveis
+meu_nome = "Rizia" # texto (string)
+minha_idade = 28 # numero inteiro (int)
+minha_altura = 1.66 # numero decimal (float)
+eu_sou_estudante = True # verdadeiro/falso (bool)
